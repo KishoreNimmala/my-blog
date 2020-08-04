58a1cfffffb29521042d717f3156e8e22a92518d
Kishore Nimmala <nkishore779@gmail.com>
2020-08-04 12:47:13 +0530

    creating blog pages

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import React from 'react';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route 
-          path="/"
-          component={HomePage}
-          exact
-        />
+        <div id="page-body">
+          <Route 
+            path="/"
+            component={HomePage}
+            exact
+          />
+          <Route path="/about" component={AboutPage}/>
+          <Route path="/articleslist" component={ArticlesList}/>
+          <Route path="/article" component={ArticlePage} />
+        </div>
       </div>
     </Router>
     
