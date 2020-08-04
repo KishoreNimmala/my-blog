9513a6d0e9830f6619b6e60e2eb3dfaf7d73cecd
Kishore Nimmala <nkishore779@gmail.com>
2020-08-04 18:58:28 +0530

    Making the articles list modular

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import articleContent from './article-content';
 import {Link} from 'react-router-dom';
 
-const ArticlesList=()=>(
-    <>
-        <h1>Articles</h1>
-        {articleContent.map((article,key)=>(
+const ArticlesList=({articles})=>{
+    return(
+        <>
+        {articles.map((article,key)=>(
             <Link className="article-list-item" key={key} to={`/article/${article.name}`}>
                 <h3>{article.title}</h3>
                 <p>{article.content[0].substring(0,150)}...</p>
@@ -13,8 +12,7 @@ const ArticlesList=()=>(
             
         ))}
     </>
-);
-
+    );
+        
+}
 export default ArticlesList;
-
-
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import articleContent from './article-content';
+import ArticlesList from '../components/ArticlesList';
+
 const ArticlePage=({match})=>{
     const name=match.params.name;
     const article=articleContent.find(article=>article.name===name);
     if(!article) return <h1>Article "{name}" does not exists!</h1>
+    
+    const otherArticles=articleContent.filter(article=>article.name!==name);
+
     return(
         <>
             <h1>{article.title}</h1>
@@ -12,6 +17,8 @@ const ArticlePage=({match})=>{
                     {paragraph}
                 </p>
             ))}
+            <h1>Other Articles</h1>
+            <ArticlesList articles={otherArticles} />
         </>
     );
 }
